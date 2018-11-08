cdff653c02a034a3c25ec20ab9287152f481ba2c
TimJ <timj@elastacloud.com>
2018-11-08 15:50:45 +0000

    Changed expand contractions and expand match to be seperate functions

--- code/preprocessing/text_preprocessing.py
+++ code/preprocessing/text_preprocessing.py
@@ -2,33 +2,76 @@ from bs4 import BeautifulSoup
 import unicodedata
 import re
 from preprocessing.contractions import CONTRACTION_MAP
-
+import nltk
+from nltk.corpus import wordnet
 
 def strip_html_tags(text):
     soup = BeautifulSoup(text, "html.parser")
     stripped_text = soup.get_text()
     return stripped_text
 
-strip_html_tags('<html><h2>Some important text</h2></html>')
-
 
 def remove_accented_chars(text):
     text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
     return text
 
+def expand_match(contraction, contraction_mapping):
+    '''Takes a match object that has matched with a key in the contraction_mapping and returns the equivelent value'''
+    match = contraction.group(0)
+    first_char = match[0]
+    expanded_contraction = contraction_mapping.get(match)\
+                            if contraction_mapping.get(match)\
+                            else contraction_mapping.get(match.lower())                       
+    expanded_contraction = first_char+expanded_contraction[1:]
+    return expanded_contraction
+
+
 def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
     
     contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), 
                                       flags=re.IGNORECASE|re.DOTALL)
-    def expand_match(contraction):
-        match = contraction.group(0)
-        first_char = match[0]
-        expanded_contraction = contraction_mapping.get(match)\
-                                if contraction_mapping.get(match)\
-                                else contraction_mapping.get(match.lower())                       
-        expanded_contraction = first_char+expanded_contraction[1:]
-        return expanded_contraction
+
         
-    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = contractions_pattern.sub(lambda x: expand_match(x, contraction_mapping), text)
     expanded_text = re.sub("'", "", expanded_text)
     return expanded_text
+
+def remove_special_characters(text, remove_digits=False, remove_underscores=True):
+    '''If remove_underscores is true, replace underscores with spaces'''
+    pattern = r'[^a-zA-z0-9\s]' if not remove_digits else r'[^a-zA-z\s]'
+    text = re.sub(pattern, '', text)
+    
+    if remove_underscores:
+        text = re.sub('_', ' ', text)
+        
+    return text
+
+def simple_stemmer(text):
+    ps = nltk.PorterStemmer()
+    text = ' '.join([ps.stem(word) for word in text.split()])
+    return text
+
+#def get_wordnet_pos(treebank_tag):
+#    if treebank_tag.startswith('J'):
+#        return wordnet.ADJ
+#    elif treebank_tag.startswith('V'):
+#        return wordnet.VERB
+#    elif treebank_tag.startswith('N'):
+#        return wordnet.NOUN
+#    elif treebank_tag.startswith('R'):
+#        return wordnet.ADV
+#    else:
+#        return wordnet.NOUN
+
+
+#def normalize_text(text):
+#    word_pos = nltk.pos_tag(nltk.word_tokenize(text))
+#    lemm_words = [lmtzr(sw[0], get_wordnet_pos(sw[1])) for sw in word_pos]
+#
+#    return [x.lower() for x in lemm_words]
+
+
+#if __name__ == '__main__':
+    
+    #my_string = expand_match("Y'all", CONTRACTION_MAP)
+    #print(my_string)
--- code/tests/preprocessing_test.py
+++ code/tests/preprocessing_test.py
@@ -1,15 +1,30 @@
 import unittest
 import preprocessing.text_preprocessing as txp
+import re
 
 class Test_TextPreprocessing(unittest.TestCase):
     
     def test_strip_html_tags(self):
-        self.assertEquals(txp.strip_html_tags('<html><h2>Some import<br>ant text</h2></html>'), 'Some important text')
+        self.assertEqual(txp.strip_html_tags('<html><h2>Some import<br>ant text</h2></html>'), 'Some important text')
 
     def test_remove_accented_chars(self):
-        self.assertEquals(txp.remove_accented_chars('Sómě Áccěntěd těxt ÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖŠÚÛÜÙÝŸŽàáâãäçèéêëìíîïñòóôõöšùúûüýÿž'), 'Some Accented text AAAAACEEEEIIIINOOOOOSUUUUYYZaaaaaceeeeiiiinooooosuuuuyyz')
+        self.assertEqual(txp.remove_accented_chars('Sómě Áccěntěd těxt ÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖŠÚÛÜÙÝŸŽàáâãäçèéêëìíîïñòóôõöšùúûüýÿž'), 'Some Accented text AAAAACEEEEIIIINOOOOOSUUUUYYZaaaaaceeeeiiiinooooosuuuuyyz')
+
+    def test_expand_match(self):
+        self.assertEqual(txp.expand_match(re.match("don't", "don't"), {"don't": "do not"}), "do not")
+        self.assertEqual(txp.expand_match(re.match("Don't", "Don't"), {"don't": "do not"}), "Do not")
+        self.assertEqual(txp.expand_match(re.match("DoN't", "DoN't"), {"don't": "do not"}), "Do not")
 
     def test_expand_contractions(self):
-        self.assertEquals(txp.expand_contractions("Y'all can't expand contractions I'd think"), 'You all cannot expand contractions I would think' )
+        self.assertEqual(txp.expand_contractions("Y'allcan't expand contractions I'd think i'd"), 'You allcannot expand contractions I would think i would')
+
+    def test_remove_special_chars(self):
+        self.assertEqual(txp.remove_special_characters('Well this was fun! What_do you think? 123#@!', remove_digits=True, remove_underscores=True), 'Well this was fun What do you think ')
+        self.assertEqual(txp.remove_special_characters('Well this was fun! What_do you think? 123#@!'), 'Well this was fun What do you think 123')
+        self.assertEqual(txp.remove_special_characters('Well this was fun! What_do you think? 123#@!', remove_underscores=False), 'Well this was fun What_do you think 123')
+    
+    def test_simple_stemmer(self):
+      self.assertEqual(txp.simple_stemmer('My system keeps crashing his crashed yesterday, ours crashes daily'), 'My system keep crash hi crash yesterday, our crash daili')  
+
 if __name__ == '__main__':
     unittest.main()
